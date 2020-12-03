98c4d1129020a694ca216961a8c0609060761269
student <student@eda-bc-04.localdomain>
2020-12-03 15:38:36 +1300

    home component and browser router

--- server/server.js
+++ server/server.js
@@ -5,6 +5,9 @@ const server = express()
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
+server.use('*',(req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 
 
 module.exports = server
