02d12bb85248d07d7b13c0f9be90ba1eb82f706a
student <student@pop-os.localdomain>
2020-12-04 11:40:06 +1300

    userProfiles shows active user

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -9,7 +9,8 @@ import Navbar from './Navbar'
 import EventDetails from './EventDetails'
 import UserProfile from './UserProfile'
 import CreateUser from './CreateUser'
-import EventDetails from './EventDetails'
+
+
 
 
 export class App extends React.Component {
@@ -28,6 +29,7 @@ export class App extends React.Component {
           <Route exact path='/events/:id' component={EventDetails} />
           <Route exact path='/users/active' component={UserProfile} />
           <Route exact path='/users/new' component= {CreateUser} />
+       
         </Switch>
       </Router>
       </>
--- client/components/UserProfile.jsx
+++ client/components/UserProfile.jsx
@@ -1,13 +1,63 @@
 import React from 'react'
-
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 
-function UserProfile (props) {
-  return (
-    <div>
-      <h1>user profile</h1>
-    </div>
-  )
+export class UserProfile extends React.Component {
+
+
+  //   handleChange = evt => {
+     
+  //   }
+
+  //   handleSubmit = evt => {
+  //       evt.preventDefault() 
+
+  //   }
+
+
+  // componentDidMount() {
+
+  // }
+
+  render() {
+
+    return (
+      this.props.activeUser ? <div>
+         <h1>Profile</h1>
+          <div>
+            <div>
+            <img src={this.props.activeUser.imgURL}  className='profilePicture' />
+            </div>
+            <h3>Username: {this.props.activeUser.username}</h3>
+            <h3>email: {this.props.activeUser.email}</h3>
+            <h3>bike type:{this.props.activeUser.bikeType}</h3>
+          </div>
+          <div>
+            <h3>Upcoming events</h3>
+            <ul>
+              <li>event</li>
+            </ul>
+          </div>
+          <div>
+            <h3>Past events</h3>
+            <ul>
+              <li></li>
+            </ul>
+          </div>
+          <Link to='/editProfile'>
+          <button>Edit profile</button>
+          </Link>
+     </div> :null
+    )
+  }
 }
 
-export default UserProfile
+function mapStateToProps(globalState) {
+  return {
+    activeUser: globalState.activeUser,
+    events: globalState.events
+
+  }
+}
+
+export default connect(mapStateToProps)(UserProfile)
