df13c03e16f1bc56ce77c7fbf126a664fdf47edc
matt-blackbourn <matt.blackie114@gmail.com>
2020-12-04 10:34:41 +1300

    users/new fully functional

--- client/actions/users.js
+++ client/actions/users.js
@@ -1,6 +1,8 @@
-import { fetchUsersAPI } from "../apis/users"
+import { fetchUsersAPI, addUserAPI } from "../apis/users"
 
 export const SET_USERS = 'SET_USERS'
+export const USER_ADDED = 'USER_ADDED'
+export const ACTIVE_USER_CHANGED = 'ACTIVE_USER_CHANGED'
 
 export const fetchUsers = () => {
   return dispatch => {
@@ -13,5 +15,29 @@ export const setUsers = users => {
   return {
     type: SET_USERS,
     users
+  }
+}
+
+export const addNewUser = (user) => {
+  return dispatch => {
+    addUserAPI(user)
+    .then(id => {
+      user.id = id
+      dispatch(addUser(user))
+    })
+  }
+}
+
+export const addUser = user => {
+  return {
+    type: USER_ADDED,
+    user
+  }
+}
+
+export const changeActiveUser = user => {
+  return {
+    type: ACTIVE_USER_CHANGED,
+    user
   }
 }
