e65e5989b8fcf9e2a44cbb0e732a2e0849cde499
matt-blackbourn <matt.blackie114@gmail.com>
2020-12-03 16:38:08 +1300

    set up /events/:id

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { fetchEvents } from '../actions/events'
 import { fetchUsers } from '../actions/users'
-import{ BrowserRouter as Router, Switch, Route } from 'react-router-dom'; 
+import{ BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import Home from './Home'
 import Navbar from './Navbar'
+import { EventDetails } from './EventDetails'
 
 
 export class App extends React.Component {
@@ -21,6 +22,7 @@ export class App extends React.Component {
         <Navbar />
         <Switch>
           <Route path='/' exact component= {Home} />
+          <Route path='/events/:id' exact component= {EventDetails} />
         </Switch>
       </Router>
       </>
--- server/server.js
+++ server/server.js
@@ -8,11 +8,12 @@ const userRoutes = require('./routes/users')
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
-server.use('*',(req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
-})
 
 server.use('/api/events', eventRoutes)
 server.use('/api/users', userRoutes)
+
+server.use('*',(req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 
 module.exports = server
