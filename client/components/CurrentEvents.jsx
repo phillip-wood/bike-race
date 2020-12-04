ef15b0fad4bb98036c0887b03eb719aadf069c9f
student <student@eda-bc-02.localdomain>
2020-12-05 10:49:30 +1300

    fixed the eventslist page

--- client/components/CurrentEvents.jsx
+++ client/components/CurrentEvents.jsx
@@ -11,20 +11,21 @@ class CurrentEvents extends React.Component {
          <ul className='event--li'>
           {this.props.events.map( event => { 
             //declears new date varible for current time/date
-           let d = new Date()
+
            // converts it into epoch time
-           let currentTime = d.getTime()
+           let currentTime = Date.now()/1000
            
            // converts epoch race time to date/time string then slice out what we need to display
            let raceFullDate = String(new Date(event.startTime * 1000))
            let raceDate = raceFullDate.slice(0,15)
            let raceTime = raceFullDate.slice(17,21)
+           console.log(currentTime < event.startTime)
 
-            if(currentTime > event.startTime){
+            if(currentTime < event.startTime){
             return (
                 <li key={event.id} 
                       >
-                    <a href={`event/${event.id}`}>
+                    <a href={`events/${event.id}`}>
                         <h2 className='eventListTitle'>{event.eventName}</h2>
                         <h4>{`Time: ${raceTime}`}</h4> 
                         <h4>{`Date: ${raceDate}`}</h4>
