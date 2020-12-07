82d5bb819795b1195dc6b0c8ac637e3b8164cf8d
matt-blackbourn <matt.blackie114@gmail.com>
2020-12-08 11:44:20 +1300

    remove initial state active user

--- client/reducers/activeUser.js
+++ client/reducers/activeUser.js
@@ -1,16 +1,16 @@
 import { ACTIVE_USER_CHANGED, USER_UPDATED, ACTIVE_USER_REMOVED } from "../actions/users"
 
-const initial = {
-  id: 3,
-    username: 'bonecrusher',
-    email: 'rose@gmail.com',
-    imgURL: 'https://alchetron.com/cdn/sarah-walker-bmx-rider-e5e6abf1-fd8c-4070-b41f-1eef9785984-resize-750.jpeg',
-    eventsAttended: null,
-    bikeType: 'road bike',
-    hash: null
-}
+// const initial = {
+//   id: 3,
+//     username: 'bonecrusher',
+//     email: 'rose@gmail.com',
+//     imgURL: 'https://alchetron.com/cdn/sarah-walker-bmx-rider-e5e6abf1-fd8c-4070-b41f-1eef9785984-resize-750.jpeg',
+//     eventsAttended: null,
+//     bikeType: 'road bike',
+//     hash: null
+// }
 
-const reducer = (state = initial, action) => {
+const reducer = (state = null, action) => {
   switch (action.type) {
 
     case ACTIVE_USER_CHANGED:
