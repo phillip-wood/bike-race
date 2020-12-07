fbdaf74986f1e39592742fecedd103a275c2ee0f
student <student@pop-os.localdomain>
2020-12-07 14:19:48 +1300

    testing

--- client/components/UserProfile.jsx
+++ client/components/UserProfile.jsx
@@ -59,11 +59,7 @@ export class UserProfile extends React.Component {
             </Link>
           </div>
         </div>
-        <div className="button" id="button-5">
-          <div id="translate"></div>
-          <Link to="/users/active/edit">Edit Profile</Link>
-        </div>
-
+    
       </div> : null
     )
   }
