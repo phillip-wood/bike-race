585d1b1d21228d71c878e5d618dab0e441102948
matt-blackbourn <matt.blackie114@gmail.com>
2020-12-09 09:17:26 +1300

    create deploy branch without seeds

--- server/db/seeds/comments.js
+++ server/db/seeds/comments.js
@@ -1,24 +1,24 @@
 exports.seed = function(knex) {
   // Deletes ALL existing entries
   return knex('comments').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('comments').insert([
-        {
-          username: 'rocketrider', 
-          comment: 'Hi all, looking top have a brezzy ride so anyone who wants ', 
-          event_id: 1
-        },
-        {
-          username: 'bigdog', 
-          comment: 'p have a brezzy ride so anyone who wants ', 
-          event_id: 2
-        },
-        {
-          username: 'someone else', 
-          comment: 'Hi ride so anyone who wants ', 
-          event_id: 2
-        },
-      ])
-    })
+    // .then(function () {
+    //   // Inserts seed entries
+    //   return knex('comments').insert([
+    //     {
+    //       username: 'rocketrider', 
+    //       comment: 'Hi all, looking top have a brezzy ride so anyone who wants ', 
+    //       event_id: 1
+    //     },
+    //     {
+    //       username: 'bigdog', 
+    //       comment: 'p have a brezzy ride so anyone who wants ', 
+    //       event_id: 2
+    //     },
+    //     {
+    //       username: 'someone else', 
+    //       comment: 'Hi ride so anyone who wants ', 
+    //       event_id: 2
+    //     },
+    //   ])
+    // })
 }
--- server/db/seeds/users_events.js
+++ server/db/seeds/users_events.js
@@ -1,14 +1,14 @@
 exports.seed = function(knex) {
   // Deletes ALL existing entries
   return knex('users_events').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('users_events').insert([
-        { user_id: 1, event_id: 1 },
-        { user_id: 2, event_id: 1 },
-        { user_id: 3, event_id: 2 },
-        { user_id: 4, event_id: 2 },
-        { user_id: 5, event_id: 2 },
-      ])
-    })
+    // .then(function () {
+    //   // Inserts seed entries
+    //   return knex('users_events').insert([
+    //     { user_id: 1, event_id: 1 },
+    //     { user_id: 2, event_id: 1 },
+    //     { user_id: 3, event_id: 2 },
+    //     { user_id: 4, event_id: 2 },
+    //     { user_id: 5, event_id: 2 },
+    //   ])
+    // })
 }
